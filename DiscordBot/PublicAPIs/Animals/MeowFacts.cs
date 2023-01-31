3b70824e36e6f8e28e6e166d2e941a76275b86f9
st0waway <lumixbd@gmail.com>
2023-01-31 18:43:19 +0200

    Cleaned Usings in APIs

--- DiscordBot/PublicAPIs/Animals/MeowFacts.cs
+++ DiscordBot/PublicAPIs/Animals/MeowFacts.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using DiscordBot.Helper;
-using DiscordBot.Models.Animals.DogAPI;
+﻿using DiscordBot.Helper;
 using DiscordBot.Models.Animals.MeowFacts;
 using Newtonsoft.Json;
 
--- DiscordBot/PublicAPIs/Animals/RandomDogAPI.cs
+++ DiscordBot/PublicAPIs/Animals/RandomDogAPI.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using DiscordBot.Helper;
+﻿using DiscordBot.Helper;
 using DiscordBot.Models.Animals.RandomDogAPI;
-using DiscordBot.Models.Animals.RandomFoxAPI;
 using Newtonsoft.Json;
 
 namespace DiscordBot.PublicAPIs.Animals
