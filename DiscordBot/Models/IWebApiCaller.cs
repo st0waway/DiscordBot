1804405d80c614ce8d3ad0893db342bb261601e7
st0waway <lumixbd@gmail.com>
2023-02-20 13:51:11 +0200

    Add IWebApiCaller method

--- DiscordBot/Models/IWebApiCaller.cs
+++ DiscordBot/Models/IWebApiCaller.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiscordBot.Models
+﻿namespace DiscordBot.Models
 {
 	internal interface IWebApiCaller
 	{
+		public string GetApiResponse();
 	}
 }
